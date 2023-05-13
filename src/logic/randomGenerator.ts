faa136e2932c18a46b9e2eda13611fa9a6ca8e0f
Lenni009 <guentherhanslphdyt@gmail.com>
2023-05-13 17:53:29 +0000

    Deploying to gh-pages from @ Lenni009/Random-Hub-Glyphs-Generator@88d1f83e6e1b6ce6018c8ceaece0fcb41b0582f9 🚀

---
+++ src/logic/randomGenerator.ts
@@ -0,0 +1,5 @@
+// from https://www.w3schools.com/js/js_random.asp
+// this JavaScript function always returns a random number between min (included) and max (excluded):
+export function getRandomNumber(min: number, max: number) {
+	return Math.floor(Math.random() * (max - min)) + min;
+}
---
+++ src/main.ts
@@ -0,0 +1,40 @@
+import './styles.css'
+import { getRandomGlyphs } from './logic/glyphs';
+import { switchTheme } from './logic/theme';
+
+interface Elements {
+	themeSwitch?: HTMLButtonElement;
+	galaxyInput?: HTMLSelectElement;
+	die?: HTMLButtonElement;
+	glyphOutput?: HTMLOutputElement;
+}
+
+const ids: { [key: string]: string } = {
+	themeSwitch: 'themeSwitch',
+	galaxyInput: 'galaxyInput',
+	die: 'die',
+	glyphOutput: 'glyphOutput',
+}
+
+const elements: Elements = {};
+
+for (const id in ids) {
+	//@ts-ignore the keys are of type any, because for some reason TS can't handle it...
+	elements[id] = document.getElementById(id);
+}
+
+// add handlers
+elements.die!.onclick = () => showRandomGlyphs();
+elements.themeSwitch!.onclick = () => switchTheme();
+
+// this is the main function that gets called when you press the button
+function showRandomGlyphs(): void {
+	const galaxy = elements.galaxyInput!.value;
+	const glyphs = getRandomGlyphs(galaxy);
+	displayGlyphs(glyphs);
+}
+
+function displayGlyphs(glyphs: string): void {
+	const output = elements.glyphOutput;
+	output!.innerText = glyphs;
+}
