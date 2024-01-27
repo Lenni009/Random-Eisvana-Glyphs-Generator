591d82e039497a3dbee446e2b29e1146b346dcd3
Lenni009 <guentherhanslphdyt@gmail.com>
2024-01-27 10:43:05 +0100

    formatting; bump packages

--- src/logic/glyphs.ts
+++ src/logic/glyphs.ts
@@ -2,28 +2,28 @@ import { getRandomNumber } from './randomGenerator';
 import { regionGlyphs } from './getRegions';
 
 export function getRandomGlyphs() {
-	const regionGlyphs = getRandomRegion();
-	const SIV = getRandomSIV();
-	const planetId = getRandomPlanet();
-	const glyphs = planetId + SIV + regionGlyphs;
-	return glyphs;
+  const regionGlyphs = getRandomRegion();
+  const SIV = getRandomSIV();
+  const planetId = getRandomPlanet();
+  const glyphs = planetId + SIV + regionGlyphs;
+  return glyphs;
 }
 
 function getRandomRegion(): string {
-	const amountOfRegions = regionGlyphs.length;
-	const randomIndex = getRandomNumber(0, amountOfRegions);
-	const chosenRegion = regionGlyphs[randomIndex];
-	return chosenRegion;
+  const amountOfRegions = regionGlyphs.length;
+  const randomIndex = getRandomNumber(0, amountOfRegions);
+  const chosenRegion = regionGlyphs[randomIndex];
+  return chosenRegion;
 }
 
 function getRandomSIV(): string {
-	const decId = getRandomNumber(1, 768)		// 768 is 300 in Hex, which is not possible as a system ID
-	const hexId = decId.toString(16).toUpperCase();
-	const SIV = hexId.padStart(3, '0');
-	return SIV;
+  const decId = getRandomNumber(1, 768); // 768 is 300 in Hex, which is not possible as a system ID
+  const hexId = decId.toString(16).toUpperCase();
+  const SIV = hexId.padStart(3, '0');
+  return SIV;
 }
 
 function getRandomPlanet(): string {
-	const planetId = getRandomNumber(1, 7);		// since the second arg is *excluding*, we need to be one higher than the maximum to include 6
-	return planetId.toString();
+  const planetId = getRandomNumber(1, 7); // since the second arg is *excluding*, we need to be one higher than the maximum to include 6
+  return planetId.toString();
 }
--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,26 @@
 import '@picocss/pico';
-import './styles.css'
+import './styles.css';
 // the order of the CSS imports is IMPORTANT, DO NOT change it!!!
 import { getRandomGlyphs } from './logic/glyphs';
 import { switchTheme } from './logic/theme';
 
 interface Elements {
-	themeSwitch?: HTMLButtonElement;
-	die?: HTMLButtonElement;
-	glyphOutput?: HTMLOutputElement;
+  themeSwitch?: HTMLButtonElement;
+  die?: HTMLButtonElement;
+  glyphOutput?: HTMLOutputElement;
 }
 
 const ids: { [key: string]: string } = {
-	themeSwitch: 'themeSwitch',
-	die: 'die',
-	glyphOutput: 'glyphOutput',
-}
+  themeSwitch: 'themeSwitch',
+  die: 'die',
+  glyphOutput: 'glyphOutput',
+};
 
 const elements: Elements = {};
 
 for (const id in ids) {
-	//@ts-ignore the keys are of type any, because for some reason TS can't handle it...
-	elements[id] = document.getElementById(id);
+  //@ts-ignore the keys are of type any, because for some reason TS can't handle it...
+  elements[id] = document.getElementById(id);
 }
 
 // add handlers
@@ -29,11 +29,11 @@ elements.themeSwitch!.onclick = () => switchTheme();
 
 // this is the main function that gets called when you press the button
 function showRandomGlyphs(): void {
-	const glyphs = getRandomGlyphs();
-	displayGlyphs(glyphs);
+  const glyphs = getRandomGlyphs();
+  displayGlyphs(glyphs);
 }
 
 function displayGlyphs(glyphs: string): void {
-	const output = elements.glyphOutput;
-	output!.innerText = glyphs;
+  const output = elements.glyphOutput;
+  output!.innerText = glyphs;
 }
